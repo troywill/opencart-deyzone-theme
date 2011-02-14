b08f8d0d463e59bda237a730684ed373034d8842
Troy Will <troydwill@gmail.com>
2011-02-14 14:35:55 -0800

    Added language files, since Minna wanted changes, especially Payment methtods

---
+++ 1.4.9.1/language/english/module/featured.php
@@ -0,0 +1,8 @@
+<?php
+// Heading 
+$_['heading_title']  = 'Featured';
+
+// Text
+$_['text_stars']     = '%s out of 5 Stars!';
+$_['text_products']  = 'Products';
+?>
---
+++ 1.4.9.1/language/english/module/latest.php
@@ -0,0 +1,8 @@
+<?php
+// Heading 
+$_['heading_title']  = 'Latest';
+
+// Text
+$_['text_stars']     = '%s out of 5 Stars!';
+$_['text_products']  = 'Products';
+?>
